7c0916665e9912b7b06532ecdebc623d2798f10f
JhonJurielGulane599 <jhongulane126@gmail.com>
2025-12-01 21:57:14 +0800

    Nabutngan nakog s ang http sorry naman hahaha

--- src/server.js
+++ src/server.js
@@ -32,8 +32,7 @@ const allowedOrigins = [
     'http://localhost:3000', // React/Next.js frontend default
     'http://127.0.0.1:8081',
     'http://127.0.0.1:8080',
-    'https://laundrolink-backend.onrender.com',
-    'https://laundrolink-admin.rf.gd',
+    'http://laundrolink-backend.onrender.com',
 ];
 
 app.use(cors({
